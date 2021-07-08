b39c80f7f7149df8e4b362d070148811e584e167
FadelKachour <fadel.kachour@estiam.com>
2021-07-08 03:12:24 +0200

    update server&json

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/game-estiam-hackaton'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/game-estiam-hackaton/'}),
 );
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
